427b8a8765edc349dd3417730784ff3364f2b118
William Berry <76190311+wberry86@users.noreply.github.com>
2021-07-25 17:47:58 -0500

    updated readme and set up display info function

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
-# weather-dashboard
+# Weather-Dashboard
 
-## Server side API's challenge
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-Apache-blue)](https://shields.io)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [Link to Website](#link)
+  - [Screenshot](#screenshot)
+  
+  ## Description:
+  This challenge was designed to utilize the server side API from [openweathermap.org](https://openweathermap.org/) in order to further solidify skills with data retreival and dynamically updating HTML and CSS with the use of javaScript. 
+  ## Installation:
+  n/a
+  ## Usage:
+  n/a
+  ## License:
+  Apache
+  [Github License](Apache)
+  ## Contributing:
+  n/a
+  ## Tests:
+  n/a
+  ## Questions:
+  Below are links to my Github page and email in case you would like to contact me or have any further questions.
+  ## Link
+  [Link to weather Dashboard](https://wberry86.github.io/weather-dashboard/)
+  ## Screenshot
+  ![weather dashboard screenshot](https://github.com/wberry86/weather-dashboard/blob/main/assets/images/weather-dashboard-capture.PNG)
 
-### This challenge was designed to utilize the server side API from [openweathermap.org](https://openweathermap.org/) in order to further solidify skills with data retreival and dynamically updating HTML and CSS with the use of javaScript. 
+  - Github: [wberry86](https://github.com/wberry86)
+  - Email: wberry86@gmail.com
 
-[Link to weather Dashboard](https://wberry86.github.io/weather-dashboard/)
-
-![weather dashboard screenshot](https://github.com/wberry86/weather-dashboard/blob/main/assets/images/weather-dashboard-capture.PNG)
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,107 +1,157 @@
-var windSpeedEl = document.querySelector("#wind-speed");
-var humidityEl = document.querySelector("#humidity");
-var uvIndexEl = document.querySelector("#uv-index");
-var cityEl = document.querySelector("#city-date");
-var weatherContainerEl = document.querySelector("#weather-container");
 var searchButton = document.getElementById("srchButton");
-var cityInput = document.getElementById("search-container");
-var cityHistory = document.getElementById("search-history");
+var cityContainerEl = document.querySelector("#city-container");
+var fiveContainerEl = document.querySelector("#five-container");
+var citySearchTerm = document.querySelector("#city-search-term");
+
 var apiKey = "fac3e98026caf2998020a1a4238bc7fb";
 
-// listener for search button to record user input
-searchButton.addEventListener("click", getUserInput, insertWeather);
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#cityname");
 
-// function to store the value of the user input
-function getUserInput() {
-  var userInput = document.getElementById("input").value;
-  console.log(userInput);
-}
-
-function insertWeather(event) {
+// function to use city serch feature to plug city name into api call
+var formSubmitHandler = function (event) {
   event.preventDefault();
-  var citySearch = document.createElement("li");
-  citySearch.textContent = cityInput.value;
-  cityHistory.appendChild(citySearch);
+  // get value from input element
+  var cityName = nameInputEl.value.trim();
+
+  if (cityName) {
+    getCityStats(cityName);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a City");
+  }
+  console.log(event);
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+var getCityStats = function (city) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    apiKey;
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayInfo(data, city);
+        
+      });
+    } else {
+      alert("Error: City Not Found");
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to OpenWeather");
+  });
+};
+
+var displayInfo = function (stats, searchTerm) {
+  // clear old content
+  cityContainerEl.textContent = "";
+  citySearchTerm.textContent = searchTerm;
+
+   // create a container for each repo
+   var day = document.createElement("div");
+   day.classList = "list-item flex-row justify-space-between align-center";
+ 
+   day.textContent = JSON.stringify(stats.city);
+
+   // append container to the dom
+   cityContainerEl.appendChild(day);
+
+  for (var i = 0; i < 5; i++) {
     
-}
+    // create a span element to hold repository name
+    var five = document.createElement("div");
+    five.classList = "list-item flex-row justify-space-between align-center";
+    five.textContent = JSON.stringify(stats.list[i].main);
+    // append to container
+    fiveContainerEl.appendChild(five);
+  
+  }
+  console.log(stats);
+  console.log(searchTerm);
+  
+};
+
+// var windSpeedEl = document.querySelector("#wind-speed");
+// var humidityEl = document.querySelector("#humidity");
+// var uvIndexEl = document.querySelector("#uv-index");
+// var cityEl = document.querySelector("#city-date");
+// var weatherContainerEl = document.querySelector
+// var cityHistory = document.getElementById("search-history");("#weather-container");
+// var cityInput = document.getElementById("search-container");
+
+// function insertWeather(event) {
+//   event.preventDefault();
+//   var citySearch = document.createElement("li");
+//   citySearch.textContent = cityInput.value;
+//   cityHistory.appendChild(citySearch);
+// }
+
+// // listener for search button to record user input
+// searchButton.addEventListener("click", getUserInput, insertWeather);
+
+// // function to store the value of the user input
+// function getUserInput() {
+//   var userInput = document.getElementById("input").value;
+//   console.log(userInput);
+// }
 
 // find a way to plug user input into web api to return specified information
 
 // function to get forecast info from weather api
-var getWeatherForecast = function (api) {
-  console.log(api);
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=" +
-    apiKey;
+// var getWeatherForecast = function (api) {
+//   console.log(api);
+//   var apiUrl =
+//     "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=" +
+//     apiKey;
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // temperature
-      let temperatureDiv = document.createElement("div");
-      // create text node
-      let divElementText = document.createTextNode(
-        data.main.temp
-      );
-      // append text node to div
-        temperatureDiv.appendChild(divElementText);
-      weatherContainerEl.appendChild(temperatureDiv)
+//   fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       // temperature
+//       let temperatureDiv = document.createElement("div");
+//       // create text node
+//       let divElementText = document.createTextNode(data.main.temp);
+//       // append text node to div
+//       temperatureDiv.appendChild(divElementText);
+//       weatherContainerEl.appendChild(temperatureDiv);
 
-      // add data sets for weather forecast here...
+//       // add data sets for weather forecast here...
 
-      // humidity
-      let humidityDiv = document.createElement("div");
-      let divhumidityText = document.createTextNode(
-        data.main.humidity  
-      );
-      humidityDiv.appendChild(divhumidityText);
-      humidityEl.appendChild(humidityDiv)
+//       // humidity
+//       let humidityDiv = document.createElement("div");
+//       let divhumidityText = document.createTextNode(data.main.humidity);
+//       humidityDiv.appendChild(divhumidityText);
+//       humidityEl.appendChild(humidityDiv);
 
-      // wind-speed
-      let windSpeedDiv = document.createElement("div");
-      let divWindSpeedText = document.createTextNode(
-        data.wind.speed  
-      );
-      windSpeedDiv.appendChild(divWindSpeedText);
-      windSpeedEl.appendChild(windSpeedDiv)
+//       // wind-speed
+//       let windSpeedDiv = document.createElement("div");
+//       let divWindSpeedText = document.createTextNode(data.wind.speed);
+//       windSpeedDiv.appendChild(divWindSpeedText);
+//       windSpeedEl.appendChild(windSpeedDiv);
 
-      // uv index
-      let uvIndexDiv = document.createElement("div");
-      let divUvIndexText = document.createTextNode(
-        data.main.uv
-      );
-      uvIndexDiv.appendChild(divUvIndexText);
-      uvIndexEl.appendChild(uvIndexDiv)
+//       // uv index
+//       let uvIndexDiv = document.createElement("div");
+//       let divUvIndexText = document.createTextNode(data.main.uv);
+//       uvIndexDiv.appendChild(divUvIndexText);
+//       uvIndexEl.appendChild(uvIndexDiv);
 
-      // city/date
-      let cityDiv = document.createElement("div");
-      let divCityText = document.createTextNode(
-        data.name
-      );
-      cityDiv.appendChild(divCityText);
-      cityEl.appendChild(cityDiv)
+//       // city/date
+//       let cityDiv = document.createElement("div");
+//       let divCityText = document.createTextNode(data.name);
+//       cityDiv.appendChild(divCityText);
+//       cityEl.appendChild(cityDiv);
+//     });
+// };
 
+// document.getElementById("srchButton").addEventListener("click", function () {
+//   document.getElementById("card-deck").innerHTML = "weather goes here";
+// });
 
-    });
-};
-
-document.getElementById("srchButton").addEventListener("click", function () {
-  document.getElementById("card-deck").innerHTML = "weather goes here";
-});
-
-
-
-getWeatherForecast("weather");
-
-
-
-
-
-
-
-
-
+// getWeatherForecast("weather");
 
 /*
 // create div element
